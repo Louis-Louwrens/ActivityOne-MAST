f288632774c8114bd1aa33b56987f1acf2911318
Louis-Louwrens <ST10497905@vcconnect.edu.za>
2025-08-08 15:05:06 +0200

    Changes made in Class. Fri 08/08/2025

--- App.tsx
+++ App.tsx
@@ -1,22 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView, StyleSheet,TextInput,Button,Image,View} from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const [Name, setName] = useState('');
+  const [Surname, setSurname] = useState('');
+  console.log("App starting up now.")
   return (
-    <View style={styles.container}>
-      <Text>ActivityOne LJ Louwrens.</Text>
-      <Text>Hello World! I can code on all phones now</Text>
+    <View>
+      <View style={styles.mainPicture}>
+        <Image style={styles.ImageSize}
+        source={require('./assets/reactNativeImage.png')}/>
+      </View>
+          
+        <Text style={styles.welcomeText}> Welcome your react app!</Text>
 
-      <StatusBar style="auto" />
+      <View style={styles.InputFlex}>
+        <Text style={styles.HeadingText} >Enter Name:</Text>
+        <TextInput style={styles.InputBoxs}
+                   placeholder="First Name :"
+                   onChangeText={newText => setName(newText)}
+                   />
+      </View>
+      <View style={styles.InputFlex}>
+        <Text style={styles.HeadingText}>Enter Surname: </Text>
+        <TextInput style={styles.InputBoxs} 
+                    placeholder="Surname :"
+                    onChangeText={newText => setSurname(newText)}/>
+      </View>
+        <Button title = "Add User" onPress={()=>{console.log("The users name is: " + Name + " Surname: " + Surname)}}/>
     </View>
+       
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  welcomeText: {
+    paddingTop: 40,
+    color: 'purple',
+    fontWeight: 'bold',
+    fontSize: 28,
+    textAlign: 'center'
   },
+
+  ImageSize: {
+    width: 350,
+    height: 350
+  },
+
+  mainPicture: {
+    paddingTop: 40,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+
+  InputFlex:{
+    flexDirection:'row',
+    marginTop: 30,
+    justifyContent: 'space-evenly',
+  }
 });
